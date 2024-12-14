6e4605b3350897dbe75871229ba59795141cad30
Arturocs160 <kronculd@outlook.com>
2024-12-14 16:52:08 -0600

    Correcion en la ubicacion del footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,36 +10,46 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-            <link href="./output.css" rel="stylesheet"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <link rel="stylesheet" href="css/image.css" />
+        <link href="./output.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/image.css" />
       </head>
       <body>
         <header>
-        <div className="text-white absolute z-10 w-full">
-                <div className=" flex flex-row justify-around bg-red-700 py-9 ">
-                    <div className="flex flex-rows h-9 items-center">
-                        <p>LOGO</p>
-                    </div >
-                    <div className="flex flex-rows h-9 items-center">
-                        <img src="assets/location.png" alt="" width="35px" className="pr-2"/>
-                        <p>Direccion </p>
-                    </div>
-                    <div className="flex flex-rows h-9 items-center">
-                        <img src="assets/phone.png" alt="" width="35px" className="pr-2" />
-                        <p>Telefono </p>
-                    </div>
-                </div>
-               <div className="flex w-full justify-center">
-                <div className="flex flex-row  justify-around bg-black text-white w-3/4 h-14 items-center relative bottom-5  shadow-xl" >
-                        <a href="/">INICIO</a>
-                        <a href="/">NOSOTROS</a>
-                        <a href="/">SERVICIOS</a>
-                        <a href="/">PROYECTOS</a>
-                        <a href="/">CONTACTO</a>
-                    </div>
-               </div>
+          <div className="text-white absolute z-10 w-full">
+            <div className=" flex flex-row justify-around bg-red-700 py-9 ">
+              <div className="flex flex-rows h-9 items-center">
+                <p>LOGO</p>
+              </div>
+              <div className="flex flex-rows h-9 items-center">
+                <img
+                  src="assets/location.png"
+                  alt=""
+                  width="35px"
+                  className="pr-2"
+                />
+                <p>Direccion </p>
+              </div>
+              <div className="flex flex-rows h-9 items-center">
+                <img
+                  src="assets/phone.png"
+                  alt=""
+                  width="35px"
+                  className="pr-2"
+                />
+                <p>Telefono </p>
+              </div>
             </div>
+            <div className="flex w-full justify-center">
+              <div className="flex flex-row  justify-around bg-black text-white w-3/4 h-14 items-center relative bottom-5  shadow-xl">
+                <a href="/">INICIO</a>
+                <a href="/">NOSOTROS</a>
+                <a href="/">SERVICIOS</a>
+                <a href="/">PROYECTOS</a>
+                <a href="/">CONTACTO</a>
+              </div>
+            </div>
+          </div>
           {/* <header>
               <Image
                 src="/images.png"
@@ -49,41 +59,43 @@ export default function RootLayout({ children }) {
               ></Image>
           </header> */}
         </header>
-        <footer>
-        <div className=" flex flex-row justify-around py-5 bg-red-700 text-white" >
-                <div >
-                    <h1 className="pb-3 pt-2 text-lg">Perforación Direccional de México</h1>
-                    <div className="flex flex-row py-2 items-center">
-                        <img src="assets/location.png" alt="" width="25px" />
-                        <p className="px-2">Dirección</p>
-                    </div>
-                    <div className="flex flex-row py-2 items-center">
-                        <img src="assets/phone.png" alt="" width="25px" />
-                        <p className="px-2">Telefono</p>
-                    </div>
-                    <div className="flex flex-row py-2 items-center">
-                        <img src="assets/gmail.png" alt="" width="25px" />
-                        <p className="px-2">correo</p>
-                    </div>
-                </div>
-                <div>
-                    <h1 className="pb-3 pt-2 text-lg">  Secciones</h1>
-                    <p>Inicio</p>
-                    <p>Nosotros</p>
-                    <p>Servicios</p>
-                    <p>Proyectos</p>
-                    <p>Contacto</p>
-                </div>
-                <div>
-                    <h1 className="pb-3 pt-2 text-lg">Servicios</h1>
-                    <p>Perforación Direccional Dirigida</p>
-                    <p>Georradar</p>
-                    <p>Cracking</p>
-                    <p>Termofusión</p>
-                </div>
-            </div>
-        </footer>
         {children}
+        <footer>
+          <div className=" flex flex-row justify-around py-5 bg-red-700 text-white">
+            <div>
+              <h1 className="pb-3 pt-2 text-lg">
+                Perforación Direccional de México
+              </h1>
+              <div className="flex flex-row py-2 items-center">
+                <img src="assets/location.png" alt="" width="25px" />
+                <p className="px-2">Dirección</p>
+              </div>
+              <div className="flex flex-row py-2 items-center">
+                <img src="assets/phone.png" alt="" width="25px" />
+                <p className="px-2">Telefono</p>
+              </div>
+              <div className="flex flex-row py-2 items-center">
+                <img src="assets/gmail.png" alt="" width="25px" />
+                <p className="px-2">correo</p>
+              </div>
+            </div>
+            <div>
+              <h1 className="pb-3 pt-2 text-lg"> Secciones</h1>
+              <p>Inicio</p>
+              <p>Nosotros</p>
+              <p>Servicios</p>
+              <p>Proyectos</p>
+              <p>Contacto</p>
+            </div>
+            <div>
+              <h1 className="pb-3 pt-2 text-lg">Servicios</h1>
+              <p>Perforación Direccional Dirigida</p>
+              <p>Georradar</p>
+              <p>Cracking</p>
+              <p>Termofusión</p>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
---
+++ src/app/proyectos/proyecto/page.js
@@ -0,0 +1,42 @@
+export default function ProyectoPage() {
+  return (
+    <div>
+      <div className="pt-40">
+        <div className="pl-10 text-black text-4xl font-bold">
+          <h1>PROYECTOS</h1>
+          <hr className="w-2/5 border-2 border-black" />
+        </div>
+        <div className="pl-10 py-5 text-black text-4xl font-bold">
+          <h1>TITULO</h1>
+        </div>
+        <div className="flex flex-row ">
+          <div className=" w-1/2 px-5 grid grid-cols-2 grid-rows-2 gap-4">
+            <div className="col-span-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quo
+              architecto vitae! Quod dolores illum officiis vero alias velit
+              iure nobis blanditiis inventore asperiores mollitia similique
+              ipsa, officia laboriosam vitae!
+            </div>
+            <div className="row-start-2">
+              <img src="assets/fondo.jpg" alt="" />
+            </div>
+            <div className="row-start-2">
+              <img src="assets/fondo.jpg" alt="" />
+            </div>
+          </div>
+          <div className="w-1/2 px-5 grid grid-cols-2 grid-rows-2 gap-4">
+            <div className="col-span-2">
+              <img src="assets/fondo.jpg" alt="" />
+            </div>
+            <div className="row-start-2">
+              <img src="assets/fondo.jpg" alt="" />
+            </div>
+            <div className="row-start-2">
+              <img src="assets/fondo.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
